7404cf06c53e02a20e2bb870d26343d977261c04
Stanislav <63556577+starovoytov-s@users.noreply.github.com>
2020-10-14 17:57:01 +0300

    Initial Commit

---
+++ Twitter/Controller/MainTabController.swift
@@ -0,0 +1,19 @@
+//
+//  MainTabController.swift
+//  Twitter
+//
+//  Created by Stanislav Starovoytov on 14.10.2020.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
