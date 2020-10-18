18a3a91c97305ee32147925847fd42346f25d39e
Stanislav <63556577+starovoytov-s@users.noreply.github.com>
2020-10-18 17:01:14 +0300

    Part_05 Creating tweets

--- Twitter/Controller/MainTabController.swift
+++ Twitter/Controller/MainTabController.swift
@@ -88,7 +88,11 @@ class MainTabController: UITabBarController {
     // MARK: - Selectors
     
     @objc func actionButtonTapped() {
-        print(111)
+        guard let user = user else { return }
+        let controller = UploadTweetController(user: user)
+        let nav = UINavigationController(rootViewController: controller)
+        nav.modalPresentationStyle = .fullScreen
+        present(nav, animated: true, completion: nil)
     }
 
     
---
+++ Twitter/View/CaptionTextView.swift
@@ -0,0 +1,54 @@
+//
+//  CaptionTextView.swift
+//  Twitter
+//
+//  Created by Stanislav Starovoytov on 18.10.2020.
+//
+
+import UIKit
+
+// MARK: - Properties
+
+class CaptionTextView: UITextView {
+    let placeholderLabel: UILabel = {
+       let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textColor = .darkGray
+        label.text = "What's happening?"
+        return label
+        
+    }()
+    
+    
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect, textContainer: NSTextContainer?) {
+        super.init(frame: frame, textContainer: textContainer)
+        
+        backgroundColor = .white
+        font = UIFont.systemFont(ofSize: 16)
+        isScrollEnabled = false
+        heightAnchor.constraint(equalToConstant: 300).isActive = true
+        
+        addSubview(placeholderLabel)
+        placeholderLabel.anchor(top: topAnchor, left: leftAnchor, paddingTop: 8, paddingLeft: 4)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(handleTextInputChange), name: UITextView.textDidChangeNotification, object: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    // MARK: - Selectors
+    
+    @objc func handleTextInputChange() {
+        
+        placeholderLabel.isHidden = !text.isEmpty
+        
+    }
+    
+    
+}
